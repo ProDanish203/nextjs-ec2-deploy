e61059bc53918ea63550443c0f06acd770fd3c37
Danish <danishsidd524@gmail.com>
2024-08-19 03:56:19 +0500

    fix: Continous Deployment

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="container min-h-screen py-24 mx-auto">
       <div>
         <h1 className="text-white text-center text-4xl">
-          NextJs Application for deployment
+          NextJs Application for deployment with CI/CD pipeline
         </h1>
       </div>
     </main>
